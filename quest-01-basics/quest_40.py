4ded0c1d6818922f589f7f5fc01be66ef145ad76
Nikcael Lopes <nikcael.lopes.124@ufrn.edu.br>
2026-09-10 20:55:54 -0300

    add: adiciona a quest_40.py

---
+++ quest-01-basics/quest_40.py
@@ -0,0 +1,9 @@
+a = int(input("Digite o valor de A: "))
+b = int(input("Digite o valor de B: "))
+
+if a<b:
+    print("B maior que A")
+elif a>b:
+    print("A maior que B")
+elif a==b:
+    print("Os dois valores são iguais")
